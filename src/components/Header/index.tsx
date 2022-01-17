ccffe984db6b678137811f3263fdb419f02c692d
Antonia <antonia@unifesspa.edu.br>
2022-01-16 23:06:12 -0300

    cria modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,9 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
-
 interface HeaderProps{
     onOpenNewTransactionModal: ()=>void;
 }
-
 export function Header({onOpenNewTransactionModal}: HeaderProps){
 
     return (
